06a6ae4da5793d9fc781a132c13cc1e5c19f3416
Andi <andirizky.bayuputra@gmail.com>
2022-07-01 20:58:14 +0700

    project 1

---
+++ public/scanData/62bef9f500dc0PH.php
@@ -0,0 +1,8 @@
+<?php
+                        session_start();
+                        if($_SESSION['login'] === true){
+                                $UIDresult= '8.07';
+                                echo $UIDresult; 
+                            }
+                        ?>
+                    
---
+++ public/scanData/62bef9f500dc0SUHU.php
@@ -0,0 +1,8 @@
+<?php
+                        session_start();
+                        if($_SESSION['login'] === true){
+                                $UIDresult= '26.63';
+                                echo $UIDresult; 
+                            }
+                        ?>
+                    
